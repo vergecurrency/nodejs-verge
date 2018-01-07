91fc9c9b3cb7d5a3ea99b35a7005c95d4e215a66
Stan Kirdey <stan@Leticias-iMac.local>
2018-01-06 18:02:05 -0800

    a few simple unit tests

--- test/test-commands-verge.js
+++ test/test-commands-verge.js
@@ -1,5 +1,11 @@
+var client = require('../lib/verge')();
+
 exports.testClientInit = function(test) {
-    var client = require('../lib/verge');
     test.notEqual(client, null, "client shall not be null");
+    test.done();
+};
+
+exports.testClientInvalidFunction = function(test) {
+    test.throws(client.exec('unknown'), 'No such command "unknown"', "invalid command throws an error");
     test.done();
 };
